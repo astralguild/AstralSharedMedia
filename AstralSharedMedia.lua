51e19b57f2c0b504d7fa1eb77fcc6adc835a60bc
Terra <46207023+TerraDK@users.noreply.github.com>
2022-05-29 01:59:29 -0500

    1.0
    First

---
+++ AstralSharedMedia.lua
@@ -0,0 +1,191 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+
+-- Chimes
+LSM:Register("sound", "|cffa335eeChime 1|r", [[Interface\Addons\AstralSharedMedia\sound\chimesound1.ogg]])
+LSM:Register("sound", "|cffa335eeChime 2|r", [[Interface\Addons\AstralSharedMedia\sound\chimesound2.ogg]])
+LSM:Register("sound", "|cffa335eeChime 3|r", [[Interface\Addons\AstralSharedMedia\sound\chimesound3.ogg]])
+LSM:Register("sound", "|cffa335eeChime 4|r", [[Interface\Addons\AstralSharedMedia\sound\chimesound4.ogg]])
+LSM:Register("sound", "|cffa335eeChime 5|r", [[Interface\Addons\AstralSharedMedia\sound\chimesound5.ogg]])
+LSM:Register("sound", "|cffa335eeChime 6|r", [[Interface\Addons\AstralSharedMedia\sound\chimesound6.ogg]])
+LSM:Register("sound", "|cffa335eeChime 7|r", [[Interface\Addons\AstralSharedMedia\sound\chimesound7.ogg]])
+LSM:Register("sound", "|cffa335eeChime 8|r", [[Interface\Addons\AstralSharedMedia\sound\chimesound8.ogg]])
+LSM:Register("sound", "|cffa335eeChime 9|r", [[Interface\Addons\AstralSharedMedia\sound\chimesound9.ogg]])
+LSM:Register("sound", "|cffa335eeChime 10|r", [[Interface\Addons\AstralSharedMedia\sound\chimesound10.ogg]])
+LSM:Register("sound", "|cffa335eeChime 11|r", [[Interface\Addons\AstralSharedMedia\sound\chimesound11.ogg]])
+LSM:Register("sound", "|cffa335eeChime 12|r", [[Interface\Addons\AstralSharedMedia\sound\chimesound12.ogg]])
+
+-- Memes
+LSM:Register("sound", "|cffa335eeNoice|r", [[Interface\Addons\AstralSharedMedia\sound\noice.ogg]])
+LSM:Register("sound", "|cffa335eeOwen Wilson|r", [[Interface\Addons\AstralSharedMedia\sound\owenWilson.ogg]])
+LSM:Register("sound", "|cffa335eeProg|r", [[Interface\Addons\AstralSharedMedia\sound\prog.ogg]])
+LSM:Register("sound", "|cffa335eeTaco Bell|r", [[Interface\Addons\AstralSharedMedia\sound\tacobell.ogg]])
+LSM:Register("sound", "|cffa335eeDKP Minus|r", [[Interface\Addons\AstralSharedMedia\sound\dkp-minus.ogg]])
+LSM:Register("sound", "|cffa335eeBruh|r", [[Interface\Addons\AstralSharedMedia\sound\bruh.ogg]])
+LSM:Register("sound", "|cffa335eeDisgusting|r", [[Interface\Addons\AstralSharedMedia\sound\disgusting.ogg]])
+LSM:Register("sound", "|cffa335eeE|r", [[Interface\Addons\AstralSharedMedia\sound\e.ogg]])
+LSM:Register("sound", "|cffa335eeFck|r", [[Interface\Addons\AstralSharedMedia\sound\fck.ogg]])
+LSM:Register("sound", "|cffa335eeHere We Go Again|r", [[Interface\Addons\AstralSharedMedia\sound\herewegoagain.ogg]])
+LSM:Register("sound", "|cffa335eeI'm In Danger|r", [[Interface\Addons\AstralSharedMedia\sound\imindanger.ogg]])
+LSM:Register("sound", "|cffa335eeAirHorn|r", [[Interface\Addons\AstralSharedMedia\sound\airhorn.ogg]])
+LSM:Register("sound", "|cffa335eeAirHorn 2|r", [[Interface\Addons\AstralSharedMedia\sound\mlg-airhorn.ogg]])
+LSM:Register("sound", "|cffa335eeOkay|r", [[Interface\Addons\AstralSharedMedia\sound\okay-meme.ogg]])
+LSM:Register("sound", "|cffa335eeOMG|r", [[Interface\Addons\AstralSharedMedia\sound\omg.ogg]])
+LSM:Register("sound", "|cffa335eeSheesh|r", [[Interface\Addons\AstralSharedMedia\sound\sheesh.ogg]])
+LSM:Register("sound", "|cffa335eeDamage|r", [[Interface\Addons\AstralSharedMedia\sound\thats-a-lot-of-damage.ogg]])
+LSM:Register("sound", "|cffa335eeStepbro|r", [[Interface\Addons\AstralSharedMedia\sound\stepbro.ogg]])
+LSM:Register("sound", "|cffa335eeToasty|r", [[Interface\Addons\AstralSharedMedia\sound\toasty.ogg]])
+LSM:Register("sound", "|cffa335eeBababooey|r", [[Interface\Addons\AstralSharedMedia\sound\bababooey.ogg]])
+LSM:Register("sound", "|cffa335eeShoop da Whoop|r", [[Interface\Addons\AstralSharedMedia\sound\firinmahlazor.ogg]])
+LSM:Register("sound", "|cffa335eeLemme Smash|r", [[Interface\Addons\AstralSharedMedia\sound\lemmesmash.ogg]])
+LSM:Register("sound", "|cffa335eeLeeroy Jenkins|r", [[Interface\Addons\AstralSharedMedia\sound\leeroyjenkins.ogg]])
+LSM:Register("sound", "|cffa335eeNico Nico Nii|r", [[Interface\Addons\AstralSharedMedia\sound\niconiconii.ogg]])
+LSM:Register("sound", "|cffa335eeOmae Wa Mou|r", [[Interface\Addons\AstralSharedMedia\sound\omaewamou.ogg]])
+LSM:Register("sound", "|cffa335eeNani|r", [[Interface\Addons\AstralSharedMedia\sound\nani.ogg]])
+LSM:Register("sound", "|cffa335eeCan't Believe|r", [[Interface\Addons\AstralSharedMedia\sound\cantbelieve.ogg]])
+LSM:Register("sound", "|cffa335eePoggers|r", [[Interface\Addons\AstralSharedMedia\sound\senko-poggers.ogg]])
+LSM:Register("sound", "|cffa335eeBronCena|r", [[Interface\Addons\AstralSharedMedia\sound\broncena.ogg]])
+LSM:Register("sound", "|cffa335eeBoom Headshot|r", [[Interface\Addons\AstralSharedMedia\sound\Boom Headshot.ogg]])
+LSM:Register("sound", "|cffa335eeTroy & Abed|r", [[Interface\Addons\AstralSharedMedia\sound\troyabed.ogg]])
+
+-- Misc
+LSM:Register("sound", "|cffa335eeAnime Moan|r", [[Interface\Addons\AstralSharedMedia\sound\animemoan.ogg]])
+LSM:Register("sound", "|cffa335eeMichael Scott No|r", [[Interface\Addons\AstralSharedMedia\sound\michaelscott_nogod.ogg]])
+LSM:Register("sound", "|cffa335eeKelly Clarkson|r", [[Interface\Addons\AstralSharedMedia\sound\kelly-clarkson.ogg]])
+LSM:Register("sound", "|cffa335eeHot Hot Hot Hot|r", [[Interface\Addons\AstralSharedMedia\sound\hothothothot.ogg]])
+LSM:Register("sound", "|cffa335eeNoob|r", [[Interface\Addons\AstralSharedMedia\sound\noob.ogg]])
+LSM:Register("sound", "|cffa335eeNoot Noot|r", [[Interface\Addons\AstralSharedMedia\sound\nootnoot.ogg]])
+LSM:Register("sound", "|cffa335eeRoll Need|r", [[Interface\Addons\AstralSharedMedia\sound\roll-need.ogg]])
+LSM:Register("sound", "|cffa335eeOrder 66|r", [[Interface\Addons\AstralSharedMedia\sound\order66.ogg]])
+LSM:Register("sound", "|cffa335eePrice is Right|r", [[Interface\Addons\AstralSharedMedia\sound\priceisright-losing.ogg]])
+LSM:Register("sound", "|cffa335eeKamehameha|r", [[Interface\Addons\AstralSharedMedia\sound\kamehameha.ogg]])
+LSM:Register("sound", "|cffa335eePickle Rick|r", [[Interface\Addons\AstralSharedMedia\sound\picklerick.ogg]])
+LSM:Register("sound", "|cffa335eeMr. Meeseeks|r", [[Interface\Addons\AstralSharedMedia\sound\rm-mrmeeseeks.ogg]])
+LSM:Register("sound", "|cffa335eeOh No|r", [[Interface\Addons\AstralSharedMedia\sound\familyguy-ohno.ogg]])
+LSM:Register("sound", "|cffa335eeZoidberg|r", [[Interface\Addons\AstralSharedMedia\sound\zoidberg.ogg]])
+LSM:Register("sound", "|cffa335eePHub Intro|r", [[Interface\Addons\AstralSharedMedia\sound\phub-intro.ogg]])
+LSM:Register("sound", "|cffa335eeCrabRave|r", [[Interface\Addons\AstralSharedMedia\sound\crab_rave.ogg]])
+LSM:Register("sound", "|cffa335eeDoubletime|r", [[Interface\Addons\AstralSharedMedia\sound\doubletime.ogg]]) 
+
+-- Misc Video Game Sounds
+LSM:Register("sound", "|cffa335eePacman Waka|r", [[Interface\Addons\AstralSharedMedia\sound\pacman-wakawaka.ogg]])
+LSM:Register("sound", "|cffa335eePacman Death|r", [[Interface\Addons\AstralSharedMedia\sound\pacman-death.ogg]])
+LSM:Register("sound", "|cffa335eeMinecraft Oof|r", [[Interface\Addons\AstralSharedMedia\sound\oof.ogg]])
+LSM:Register("sound", "|cffa335eeAmongUs Meeting|r", [[Interface\Addons\AstralSharedMedia\sound\amongus-emergencymeeting.ogg]])
+LSM:Register("sound", "|cffa335eeAmongUs Kill|r", [[Interface\Addons\AstralSharedMedia\sound\amongus-imposterkill.ogg]])
+LSM:Register("sound", "|cffa335eeAmongUs Music|r", [[Interface\Addons\AstralSharedMedia\sound\amongus-music.ogg]])
+LSM:Register("sound", "|cffa335eeSonic Ring Drop|r", [[Interface\Addons\AstralSharedMedia\sound\sonic-ringdrop.ogg]])
+LSM:Register("sound", "|cffa335eeSonic Extra Life|r", [[Interface\Addons\AstralSharedMedia\sound\sonic-extralife.ogg]])
+LSM:Register("sound", "|cffa335eeFalcon Punch|r", [[Interface\Addons\AstralSharedMedia\sound\falconpunch.ogg]])
+LSM:Register("sound", "|cffa335eeFatality|r", [[Interface\Addons\AstralSharedMedia\sound\fatality.ogg]])
+LSM:Register("sound", "|cffa335eeHadouken|r", [[Interface\Addons\AstralSharedMedia\sound\hadouken.ogg]])
+LSM:Register("sound", "|cffa335eeHey Listen|r", [[Interface\Addons\AstralSharedMedia\sound\navi-heylisten.ogg]])
+LSM:Register("sound", "|cffa335eePikachu|r", [[Interface\Addons\AstralSharedMedia\sound\pikachu-thunderbolt.ogg]])
+LSM:Register("sound", "|cffa335eeZugZug Kick", [[Interface\Addons\AstralSharedMedia\sound\ZugZugKick.ogg]])
+
+-- Super Mario Bros
+LSM:Register("sound", "|cffa335eeSMB 1-UP|r", [[Interface\Addons\AstralSharedMedia\sound\smb_1-up.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB BreakBlock|r", [[Interface\Addons\AstralSharedMedia\sound\smb_breakblock.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB Coin|r", [[Interface\Addons\AstralSharedMedia\sound\smb_coin.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB FlagPole|r", [[Interface\Addons\AstralSharedMedia\sound\smb_flagpole.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB GameOver|r", [[Interface\Addons\AstralSharedMedia\sound\smb_gameover.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB JumpSmall|r", [[Interface\Addons\AstralSharedMedia\sound\smb_jump-small.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB JumpSuper|r", [[Interface\Addons\AstralSharedMedia\sound\smb_jump-super.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB MarioDie|r", [[Interface\Addons\AstralSharedMedia\sound\smb_mariodie.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB Pause|r", [[Interface\Addons\AstralSharedMedia\sound\smb_pause.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB Pipe|r", [[Interface\Addons\AstralSharedMedia\sound\smb_pipe.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB PowerUp|r", [[Interface\Addons\AstralSharedMedia\sound\smb_powerup.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB PowerUpAppears|r", [[Interface\Addons\AstralSharedMedia\sound\smb_powerup_appears.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB StageClear|r", [[Interface\Addons\AstralSharedMedia\sound\smb_stage_clear.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMB Stomp|r", [[Interface\Addons\AstralSharedMedia\sound\smb_stomp.ogg]])
+LSM:Register("sound", "|cffa335eeSMB Vine|r", [[Interface\Addons\AstralSharedMedia\sound\smb_vine.ogg]])
+LSM:Register("sound", "|cffa335eeSMB Warning|r", [[Interface\Addons\AstralSharedMedia\sound\smb_warning.ogg]])
+
+-- Super Mario World
+LSM:Register("sound", "|cffa335eeSMW 1-UP|r", [[Interface\Addons\AstralSharedMedia\sound\smw_1-up.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW Blargg|r", [[Interface\Addons\AstralSharedMedia\sound\smw_blargg.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW BreakBlock|r", [[Interface\Addons\AstralSharedMedia\sound\smw_break_block.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW CapeRise|r", [[Interface\Addons\AstralSharedMedia\sound\smw_cape_rise.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW DoorOpens|r", [[Interface\Addons\AstralSharedMedia\sound\smw_door_opens.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW DragonCoin|r", [[Interface\Addons\AstralSharedMedia\sound\smw_dragon_coin.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW EggHatching|r", [[Interface\Addons\AstralSharedMedia\sound\smw_egg_hatching.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW FeatherGet|r", [[Interface\Addons\AstralSharedMedia\sound\smw_feather_get.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW GoalIrisOut|r", [[Interface\Addons\AstralSharedMedia\sound\smw_goal_iris-out.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW HitWhileFlying|r", [[Interface\Addons\AstralSharedMedia\sound\smw_hit_while_flying.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW Jump|r", [[Interface\Addons\AstralSharedMedia\sound\smw_jump.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW BossCorrect|r", [[Interface\Addons\AstralSharedMedia\sound\smw_lemmy_wendy_correct.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW BossFallOutOfPipe|r", [[Interface\Addons\AstralSharedMedia\sound\smw_lemmy_wendy_falls_out_of_pipe.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW BossIncorrect|r", [[Interface\Addons\AstralSharedMedia\sound\smw_lemmy_wendy_incorrect.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW KoopaBeam|r", [[Interface\Addons\AstralSharedMedia\sound\smw_magikoopa_beam.ogg]])
+LSM:Register("sound", "|cffa335eeSMW CastleCumbles|r", [[Interface\Addons\AstralSharedMedia\sound\smw_map_castle_crumbles.ogg]])
+LSM:Register("sound", "|cffa335eeSMW MessageBlock|r", [[Interface\Addons\AstralSharedMedia\sound\smw_message_block.ogg]])
+LSM:Register("sound", "|cffa335eeSMW MidwayGate|r", [[Interface\Addons\AstralSharedMedia\sound\smw_midway_gate.ogg]])
+LSM:Register("sound", "|cffa335eeSMW Pause|r", [[Interface\Addons\AstralSharedMedia\sound\smw_pause.ogg]])
+LSM:Register("sound", "|cffa335eeSMW Pipe|r", [[Interface\Addons\AstralSharedMedia\sound\smw_pipe.ogg]])
+LSM:Register("sound", "|cffa335eeSMW PowerUp|r", [[Interface\Addons\AstralSharedMedia\sound\smw_power-up.ogg]])
+LSM:Register("sound", "|cffa335eeSMW PowerUpAppears|r", [[Interface\Addons\AstralSharedMedia\sound\smw_power-up_appears.ogg]])
+LSM:Register("sound", "|cffa335eeSMW PrincessHelp|r", [[Interface\Addons\AstralSharedMedia\sound\smw_princess_help.ogg]])
+LSM:Register("sound", "|cffa335eeSMW ItemRelease|r", [[Interface\Addons\AstralSharedMedia\sound\smw_reserve_item_release.ogg]])
+LSM:Register("sound", "|cffa335eeSMW RidingYoshi|r", [[Interface\Addons\AstralSharedMedia\sound\smw_riding_yoshi.ogg]])
+LSM:Register("sound", "|cffa335eeSMW SaveMenu|r", [[Interface\Addons\AstralSharedMedia\sound\smw_save_menu.ogg]])
+LSM:Register("sound", "|cffa335eeSMW SpinJump|r", [[Interface\Addons\AstralSharedMedia\sound\smw_spin_jump.ogg]])
+LSM:Register("sound", "|cffa335eeSMW SpringJump|r", [[Interface\Addons\AstralSharedMedia\sound\smw_spring_jump.ogg]])
+LSM:Register("sound", "|cffa335eeSMW StompBones|r", [[Interface\Addons\AstralSharedMedia\sound\smw_stomp_bones.ogg]])
+LSM:Register("sound", "|cffa335eeSMW StompKoopa|r", [[Interface\Addons\AstralSharedMedia\sound\smw_stomp_koopa_kid.ogg]])
+LSM:Register("sound", "|cffa335eeSMW StompNoDamage|r", [[Interface\Addons\AstralSharedMedia\sound\smw_stomp_no_damage.ogg]])
+LSM:Register("sound", "|cffa335eeSMW SwitchActivated|r", [[Interface\Addons\AstralSharedMedia\sound\smw_switch_activated.ogg]])
+LSM:Register("sound", "|cffa335eeSMW SwitchEnding|r", [[Interface\Addons\AstralSharedMedia\sound\smw_switch_timer_ending.ogg]])
+LSM:Register("sound", "|cffa335eeSMW Thud|r", [[Interface\Addons\AstralSharedMedia\sound\smw_thud.ogg]])
+LSM:Register("sound", "|cffa335eeSMW Vine|r", [[Interface\Addons\AstralSharedMedia\sound\smw_vine.ogg]])
+LSM:Register("sound", "|cffa335eeSMW YoshiStomp|r", [[Interface\Addons\AstralSharedMedia\sound\smw_yoshi_stomp.ogg]])
+LSM:Register("sound", "|cffa335eeSMW Yoshi Tounge|r", [[Interface\Addons\AstralSharedMedia\sound\yoshi-tongue.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW Yoshi Pam|r", [[Interface\Addons\AstralSharedMedia\sound\yoshi-pam.ogg]]) 
+LSM:Register("sound", "|cffa335eeSMW Yoshi Ow|r", [[Interface\Addons\AstralSharedMedia\sound\yoshi-ow.ogg]]) 
+
+-- Super Mario 64
+
+
+-- Overwatch
+LSM:Register("sound", "|cffa335eeOW DVa Nerf This|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eedva_nerf_this.mp3]]) 
+LSM:Register("sound", "|cffa335eeOW Genji Behind You|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eegenji_behind_you.mp3]]) 
+LSM:Register("sound", "|cffa335eeOW Genji Ult|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eegenji Ult|r.mp3]]) 
+LSM:Register("sound", "|cffa335eeOW Mercy Ult|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eemercy Ult|r.ogg]]) 
+LSM:Register("sound", "|cffa335eeOW Reinhardt Group Up|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eereinhardt_group_up.ogg]]) 
+LSM:Register("sound", "|cffa335eeOW Reinhardt We Weed a Healer|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eereinhardt_we_need_a_healer.mp3]]) 
+LSM:Register("sound", "|cffa335eeOW Soldier Get In There|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eesoldier_get_in_there.mp3]]) 
+LSM:Register("sound", "|cffa335eeOW Soldier Priority Target|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eesoldier_priority_target.mp3]]) 
+LSM:Register("sound", "|cffa335eeOW Sombra Too Close|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eesombra_too_close.ogg]]) 
+LSM:Register("sound", "|cffa335eeOW Sombra Ult|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eesombra Ult|r.ogg]]) 
+LSM:Register("sound", "|cffa335eeOW Tracer Get Out|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eetracer_get_out.mp3]]) 
+LSM:Register("sound", "|cffa335eeOW Tracer We Need a Healer|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eetracer_we_need_a_healer.mp3]]) 
+LSM:Register("sound", "|cffa335eeOW Widow Incoming|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eewid|cffa335eeincoming.mp3]]) 
+LSM:Register("sound", "|cffa335eeOW Widow Take Cover|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eewid|cffa335eetake_cover.mp3]]) 
+LSM:Register("sound", "|cffa335eeOW Widow Watch Your Step|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eewid|cffa335eewatch_your_step.mp3]]) 
+LSM:Register("sound", "|cffa335eeOW Zenyatta Time Grows Short|r", [[Interface\Addons\AstralSharedMedia\sound\|cffa335eezenyatta_time_grows_short.mp3]]) 
+
+-- Ayaya
+LSM:Register("sound", "|cffa335eeAyaya!|r", [[Interface\Addons\AstralSharedMedia\sound\Ayaya!.ogg]]) 
+LSM:Register("sound", "|cffa335eeAyaya!Ayaya!|r", [[Interface\Addons\AstralSharedMedia\sound\Ayaya!Ayaya!.ogg]]) 
+LSM:Register("sound", "|cffa335eeAyaya!Ayaya!Ayaya!|r", [[Interface\Addons\AstralSharedMedia\sound\Ayaya!Ayaya!Ayaya!.ogg]]) 
+LSM:Register("sound", "|cffa335eeAyaya|r", [[Interface\Addons\AstralSharedMedia\sound\Ayaya.ogg]]) 
+LSM:Register("sound", "|cffa335eeAyayaah|r", [[Interface\Addons\AstralSharedMedia\sound\Ayayaah.ogg]]) 
+LSM:Register("sound", "|cffa335eeAyayah|r", [[Interface\Addons\AstralSharedMedia\sound\Ayayah.ogg]]) 
+
+-- -----
+--   STATUSBAR
+-- -----
